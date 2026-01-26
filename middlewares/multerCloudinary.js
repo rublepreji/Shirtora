a876a828bdff77ed97962c14154259f31e2caafa
Ruble P Reji <rublepreji95@gmail.com>
2026-01-26 18:09:12 +0530

    Downgrade express to v4 and fix session/router issue

--- middlewares/multerCloudinary.js
+++ middlewares/multerCloudinary.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import  pkg  from 'multer-storage-cloudinary';
 import cloudinary from '../config/cloudinary.js';
+const {CloudinaryStorage}=pkg
 
 const uploadto = (foldername) => {
     const storage = new CloudinaryStorage({
